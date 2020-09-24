dfc7ad0f281791e7e7090773700e0daef2a40be5
choisungwook <kgg1959@naver.com>
2020-09-24 17:36:32 +0900

    Add testuser And PasswordEncoder

--- src/main/java/com/security/springsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/com/security/springsecurity/security/ApplicationSecurityConfig.java
@@ -1,13 +1,23 @@
 package com.security.springsecurity.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -17,5 +27,17 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails testuser = User.builder()
+                .username("testuser")
+                .password(passwordEncoder.encode("password"))
+                .roles("STUDENT")
+                .build();
+
+        return new InMemoryUserDetailsManager(testuser);
     }
 }
